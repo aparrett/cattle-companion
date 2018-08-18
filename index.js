fbd1b1fe9e740a0fe2b316f9d2f76141f18339b2
Anthony Parrett <anthonyparrett7@gmail.com>
2018-08-17 22:51:11 -0500

    Setup Client

--- index.js
+++ index.js
@@ -5,5 +5,16 @@ app.get('/', (req, res) => {
   res.send({ hello: 'world' });
 });
 
+if (process.env.NODE_ENV === 'production') {
+  // Use express to serve up production assets like main.js and main.css.
+  app.use(express.static('client/build'));
+
+  // Point all other routes to index.html.
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
