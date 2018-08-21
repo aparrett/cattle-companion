34610a28f473233c7fc479347d7edeb85d731a7a
Anthony Parrett <anthonyparrett7@gmail.com>
2018-08-20 19:07:09 -0500

    Setup PrivateRoute

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,24 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import PrivateRoute from './PrivateRoute';
 
-export default class App extends Component {
-    render() {
-        return (
-            <div>Hello World!</div>
-        );
-    }
+import Header from './Header';
+
+const Home = () => <div>Hello World</div>;
+const Login = () => <div>Login</div>;
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Route exact path="/login" component={Login} />
+          <PrivateRoute exact path="/" component={Home} />
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
+
+export default App;
---
+++ client/src/components/Header.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+const Header = () => {
+  return (
+    <div>
+      Header
+      <br />
+    </div>
+  );
+}
+
+export default Header;
