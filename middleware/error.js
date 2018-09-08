7f9c4a74f56159fe28573b9cae6172ff3105b7cf
Anthony Parrett <anthonyparrett7@gmail.com>
2018-09-08 16:37:01 -0500

    Log errors to console to debug Heroku server

--- middleware/error.js
+++ middleware/error.js
@@ -1,6 +1,7 @@
 const winston = require('winston');
 
 module.exports = function(err, req, res, next){
+  console.log('Error: ' + err);
   winston.error(err.message, err);
   res.status(500).send('The server could not be reached. Please try again later.');
 }
