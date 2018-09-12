2ec25a35db75ebc0e0145ede6fb7340721b6f95e
Anthony Parrett <anthonyparrett7@gmail.com>
2018-09-12 13:06:14 -0500

    Log path value

--- startup/routes.js
+++ startup/routes.js
@@ -26,7 +26,8 @@ module.exports = function(app) {
   
     // Point all other routes to index.html.
     app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+      console.log('Path', path.resolve('client', 'build', 'index.html'));
+      res.sendFile(path.resolve('client', 'build', 'index.html'));
     });
   }
 
