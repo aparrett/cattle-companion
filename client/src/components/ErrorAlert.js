782b15d85e20bc46218e56ae2a5594b368c1bfe7
Anthony Parrett <anthonyparrett7@gmail.com>
2018-09-24 13:04:51 -0500

    Refactor Header

---
+++ client/src/components/ErrorAlert.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { dismissError } from '../actions/error';
+import { connect } from 'react-redux';
+
+const ErrorAlert = ({ error, dismissError }) => {
+  if (!error) return null;
+
+  setTimeout(() => dismissError(), 5 * 1000);
+
+  return (
+    <div>
+      <div className="alert alert-secondary">
+        { error }
+        <button type="button" className="close" onClick={() => dismissError()}> 
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+    </div>
+  );
+}
+
+function mapStateToProps({ error }) {
+  return { error };
+}
+
+export default connect(mapStateToProps, { dismissError })(ErrorAlert);
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,30 +1,9 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { dismissError } from '../actions/error';
 import { Link } from 'react-router-dom';
+import ErrorAlert from './ErrorAlert';
 
-const Header = props => {
-  function handleDismissClick() {
-    props.dismissError();
-  }
-
-  function renderError() {
-    if (props.error) {
-      setTimeout(() => props.dismissError(), 5 * 1000);
-
-      return (
-        <div>
-          <div className="alert alert-secondary">
-            { props.error }
-            <button type="button" className="close" onClick={handleDismissClick.bind(this)}> 
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-        </div>
-      );
-    }
-  }
-
+const Header = ({ authenticated }) => {
   return (
     <div>
       <div className="d-flex p-3 px-md-4 bg-white border-bottom box-shadow">
@@ -32,7 +11,7 @@ const Header = props => {
           <div className="text-center">
             <Link className="logo" to="/">Cattle Companion</Link>
           </div>
-          {props.authenticated &&
+          {authenticated &&
             <nav className="text-center">
               <Link className="mr-2" to="/">Farms</Link>
               <Link to="/login">Logout</Link>
@@ -40,13 +19,13 @@ const Header = props => {
           }
         </div>
       </div>
-      { renderError() }
+      <ErrorAlert />
     </div>
   );
 }
 
-function mapStateToProps({ error, auth: { authenticated } }) {
-  return { error, authenticated };
+function mapStateToProps({ auth: { authenticated } }) {
+  return { authenticated };
 }
 
-export default connect(mapStateToProps, { dismissError })(Header);
+export default connect(mapStateToProps)(Header);
